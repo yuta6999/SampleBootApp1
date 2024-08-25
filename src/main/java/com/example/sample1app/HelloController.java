b2904c35cfd77709221a6dd938b81973111a89f7
yuta6999 <48405817+yuta6999@users.noreply.github.com>
2024-08-25 12:09:22 +0900

    ▼リスト3-8 ▼リスト3-9 フォームコントロール

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -10,18 +10,38 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloController {
   
+//	▼リスト3-9
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView index(ModelAndView mav) {
-	  mav.addObject("msg","名前を書いて下さい。");
 	  mav.setViewName("index");
-	  return mav;
+	  mav.addObject("msg","フォームを送信下さい。");
+	return mav;
 	}
 
 	@RequestMapping(value="/", method=RequestMethod.POST)
-	public ModelAndView form(@RequestParam("text1") String str, 
+	public ModelAndView form(
+	    @RequestParam(value="check1",required=false)boolean check1,
+	    @RequestParam(value="radio1",required=false)String radio1,
+	    @RequestParam(value="select1",required=false)String select1,
+	    @RequestParam(value="select2",required=false)String[] select2,
 	    ModelAndView mav) {
-	  mav.addObject("msg","こんにちは、" + str + "さん！");
-	  mav.addObject("value",str);
+
+	  String res = "";
+	  try {
+	    res = "check:" + check1 +
+	    " radio:" + radio1 +
+	    " select:" + select1 + 
+	    "\nselect2:";
+	  } catch (NullPointerException e) {}
+	  try {
+	    res += select2[0];
+	    for(int i = 1;i < select2.length;i++) {
+	      res += ", " + select2[i];
+	    }
+	  } catch (NullPointerException e) {
+	    res += "null";
+	  }
+	  mav.addObject("msg",res);
 	  mav.setViewName("index");
 	  return mav;
 	}
