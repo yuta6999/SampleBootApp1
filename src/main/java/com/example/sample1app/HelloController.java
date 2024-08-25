4b314db5e0e038593a3a602bdcfaadfeb135c47f
yuta6999 <48405817+yuta6999@users.noreply.github.com>
2024-08-25 12:18:14 +0900

    ▼リスト3-10,11フォワードとリダイレクト

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -1,59 +1,31 @@
 package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView; 
 
 @Controller
 public class HelloController {
   
-//	▼リスト3-9
+//	▼リスト3-11
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView index(ModelAndView mav) {
 	  mav.setViewName("index");
-	  mav.addObject("msg","フォームを送信下さい。");
+	  mav.addObject("msg","HelloController/indexのページです。");
 	return mav;
 	}
 
-	@RequestMapping(value="/", method=RequestMethod.POST)
-	public ModelAndView form(
-	    @RequestParam(value="check1",required=false)boolean check1,
-	    @RequestParam(value="radio1",required=false)String radio1,
-	    @RequestParam(value="select1",required=false)String select1,
-	    @RequestParam(value="select2",required=false)String[] select2,
-	    ModelAndView mav) {
-
-	  String res = "";
-	  try {
-	    res = "check:" + check1 +
-	    " radio:" + radio1 +
-	    " select:" + select1 + 
-	    "\nselect2:";
-	  } catch (NullPointerException e) {}
-	  try {
-	    res += select2[0];
-	    for(int i = 1;i < select2.length;i++) {
-	      res += ", " + select2[i];
-	    }
-	  } catch (NullPointerException e) {
-	    res += "null";
-	  }
-	  mav.addObject("msg",res);
-	  mav.setViewName("index");
-	  return mav;
+	@RequestMapping("/other")
+	public String other() {
+	  return "redirect:/";
+	  // リダイレクト：アクセスするアドレスそのものが移動先のものに変更される
 	}
-	
-	@RequestMapping("/{temp}")
-	public String index(@PathVariable String temp) {
-	  switch(temp) {
-	    case "index":
-	    return "index";
-	    default:
-	    return "other";
-	  }
+
+	@RequestMapping("/home")
+	public String home() {
+	  return "forward:/";
+	  // フォワード：アドレスの変更なし（表示内容のみ差し替え）
 	}
 }
 
