625e2a0c7970a61240c84e6b2ef8709195b0bbda
yuta6999 <48405817+yuta6999@users.noreply.github.com>
2024-08-25 12:02:21 +0900

    3-1 フォームを利用する

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -1,13 +1,31 @@
 package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView; 
 
 @Controller
 public class HelloController {
   
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+	  mav.addObject("msg","名前を書いて下さい。");
+	  mav.setViewName("index");
+	  return mav;
+	}
+
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView form(@RequestParam("text1") String str, 
+	    ModelAndView mav) {
+	  mav.addObject("msg","こんにちは、" + str + "さん！");
+	  mav.addObject("value",str);
+	  mav.setViewName("index");
+	  return mav;
+	}
+	
 	@RequestMapping("/{temp}")
 	public String index(@PathVariable String temp) {
 	  switch(temp) {
@@ -16,13 +34,6 @@ public class HelloController {
 	    default:
 	    return "other";
 	  }
-	}
-
-	@RequestMapping()
-	public String index(Model model) {
-	model.addAttribute("msg", 
-	   "これはコントローラーに用意したメッセージです。");
-	return "index";
 	}
 }
 
