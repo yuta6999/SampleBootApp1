1ed5a538ba486797bf8643fe125a3226d26e53bf
yuta6999 <48405817+yuta6999@users.noreply.github.com>
2024-08-25 12:28:37 +0900

    ▼リスト3-12,13 th:if

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -2,30 +2,19 @@ package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView; 
 
+//▼リスト3-12
 @Controller
 public class HelloController {
+  private boolean flag = false;
   
-//	▼リスト3-11
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public ModelAndView index(ModelAndView mav) {
-	  mav.setViewName("index");
-	  mav.addObject("msg","HelloController/indexのページです。");
-	return mav;
-	}
-
-	@RequestMapping("/other")
-	public String other() {
-	  return "redirect:/";
-	  // リダイレクト：アクセスするアドレスそのものが移動先のものに変更される
-	}
-
-	@RequestMapping("/home")
-	public String home() {
-	  return "forward:/";
-	  // フォワード：アドレスの変更なし（表示内容のみ差し替え）
-	}
+  @RequestMapping("/")
+  public ModelAndView index(ModelAndView mav) {
+    flag = !flag;
+    mav.setViewName("index");
+    mav.addObject("flag", flag);
+    mav.addObject("msg", "サンプルのメッセージです。");
+    return mav;
+  }
 }
-
