38ed28e3521677bb8537b0b970df6ab9bf52492a
yuta6999 <48405817+yuta6999@users.noreply.github.com>
2024-08-26 22:13:49 +0900

    ▼リスト4-7 データの初期化

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -1,9 +1,16 @@
-//▼リスト4-3
+//▼リスト4-6
 package com.example.sample1app;
+
+import java.util.List;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.sample1app.repositories.PersonRepository;
@@ -11,18 +18,50 @@ import com.example.sample1app.repositories.PersonRepository;
 @Controller
 public class HelloController {
   
-	// Beanオブジェクトに関連付けする
   @Autowired
   PersonRepository repository;
 
   @RequestMapping("/")
-  public ModelAndView index(ModelAndView mav) {
+  public ModelAndView index(
+      @ModelAttribute("formModel") Person Person,
+      ModelAndView mav) {
     mav.setViewName("index");
     mav.addObject("title", "Hello page");
     mav.addObject("msg","this is JPA sample data.");
-   Iterable<Person> list = repository.findAll();
+    List<Person> list = repository.findAll();
     mav.addObject("data",list);
     return mav;
   }
+
+  @RequestMapping(value = "/", method = RequestMethod.POST)
+  @Transactional
+  public ModelAndView form(
+      @ModelAttribute("formModel") Person Person, 
+      ModelAndView mav) {
+    repository.saveAndFlush(Person);
+    return new ModelAndView("redirect:/");
+  }
+	
+	@PostConstruct
+	public void init(){
+	  // １つ目のダミーデータ作成
+	  Person p1 = new Person();
+	  p1.setName("taro");
+	  p1.setAge(39);
+	  p1.setMail("taro@yamada");
+	  repository.saveAndFlush(p1);
+	  // ２つ目のダミーデータ作成
+	  Person p2 = new Person();
+	  p2.setName("hanako");
+	  p2.setAge(28);
+	  p2.setMail("hanako@flower");
+	  repository.saveAndFlush(p2);
+	  // ３つ目のダミーデータ作成
+	  Person p3 = new Person();
+	  p3.setName("sachiko");
+	  p3.setAge(17);
+	  p3.setMail("sachico@happy");
+	  repository.saveAndFlush(p3);
+	}
 
 }
